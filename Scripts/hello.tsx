ef5cc5568a3d12f042c5cfcf0704eaf8c42fc0b6
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-03-16 01:09:19 +0700

    Setup TypeScript for React

---
+++ Scripts/hello.tsx
@@ -0,0 +1,21 @@
+﻿/// <reference path="../typings/main.d.ts" />
+
+import React = __React;
+
+module Hello {
+    export class HelloWorld {
+        constructor() {
+            var a = 100;
+            var b = 200;
+            var c = 300;
+            var d = 400;
+            var e = 500;
+            var f = 500;
+            var e = 600;
+        }
+
+        hello() {
+            return "Hello, world!!!";
+        }
+    }
+}
---
+++ wwwroot/ts/hello.js
@@ -0,0 +1,22 @@
+/// <reference path="../typings/main.d.ts" />
+var React = __React;
+var Hello;
+(function (Hello) {
+    var HelloWorld = (function () {
+        function HelloWorld() {
+            var a = 100;
+            var b = 200;
+            var c = 300;
+            var d = 400;
+            var e = 500;
+            var f = 500;
+            var e = 600;
+        }
+        HelloWorld.prototype.hello = function () {
+            return "Hello, world!!!";
+        };
+        return HelloWorld;
+    }());
+    Hello.HelloWorld = HelloWorld;
+})(Hello || (Hello = {}));
+//# sourceMappingURL=hello.js.map
